b377928ef47da6d35606671c34f269340bd91e16
zyamaramag <mgadielzya@gmail.com>
2026-03-19 11:55:14 +0800

    Fixing the folders

--- projectsite/asgi.py
+++ projectsite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projectsite project.
+ASGI config for hangarin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hangarin.settings')
 
 application = get_asgi_application()
--- projectsite/wsgi.py
+++ projectsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectsite project.
+WSGI config for hangarin project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hangarin.settings')
 
 application = get_wsgi_application()
